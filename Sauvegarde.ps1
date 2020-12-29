efab4f4160c8a931dca2004cd9d90069f2aa982c
JE-888 <75142556+JE-888@users.noreply.github.com>
2020-12-29 13:49:29 +0100

    Delete Sauvegarde.ps1

--- Sauvegarde.ps1
+++
@@ -1,5 +0,0 @@
-$TargetPath = Read-Host -Prompt "Enter the path of the folder you want to save "
-$FolderName = Read-Host -Prompt "Enter the name of the save folder "
-Set-Location -Path $TargetPath
-
-Compress-Archive -Path $TargetPath -CompressionLevel "Fastest" -DestinationPath $FolderName -Force
